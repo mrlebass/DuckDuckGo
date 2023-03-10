4334dfa4a555c30577ecfaae445c30c6a1c2752f
mrlebass <jubszestevao@gmail.com>
2023-03-10 20:05:43 -0300

    config file fixes

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,37 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+  clientCertificates: [
+    {
+      url: 'https://a.host.com',
+      ca: ['certs/ca.pem'],
+      certs: [
+        {
+          cert: 'certs/cert.pem',
+          key: 'certs/private.key',
+          passphrase: 'certs/pem-passphrase.txt',
+        },
+      ],
     },
-  },
-});
+    {
+      url: 'https://b.host.com/a_base_route/**',
+      ca: [],
+      certs: [
+        {
+          pfx: '/home/tester/certs/cert.pfx',
+          passphrase: '/home/tester/certs/pfx-passphrase.txt',
+        },
+      ],
+    },
+    {
+      url: 'https://a.host.*.com/',
+      ca: [],
+      certs: [
+        {
+          pfx: 'certs/cert.pfx',
+          passphrase: 'certs/pfx-passphrase.txt',
+        },
+      ],
+    },
+  ],
+})

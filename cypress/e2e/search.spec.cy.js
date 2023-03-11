a0c7458a0127a9d29143d25aa0f7d299f0bb989a
mrlebass <leandrodlestevao@hotmail.com>
2023-03-10 21:14:48 -0300

    code programming improvement

--- cypress/e2e/search.spec.cy.js
+++ cypress/e2e/search.spec.cy.js
@@ -1,28 +1,29 @@
-/// <reference types="cypress" />
+//<reference types="cypress" />
 import searchData from '../fixtures/searchData.json'
 
 
 /* Uso de BDD sem usar Cucumber
 *
-* Como um usuário da world-wide web
-* Quero realizar buscas das últimas notícias sobre Bitcoin
+* Como um usuario da world-wide web
+* Quero realizar buscas das ultimas noticias sobre Bitcoin
 * Para aprofundar meus conhecimentos em Bitcoins
 */
 
-describe('Casos de Teste - Busca na página DuckDuckGo', () => {
+describe('Casos de Teste - Busca na pagina DuckDuckGo', () => {
 
   beforeEach(() => {
-    cy.visit('https://duckduckgo.com/') // comando para visitar a página desejada
+    cy.visit('http://duckduckgo.com/') // comando para visitar a pagina desejada
+    cy.get('.ddgsi.ddgsi-horn')
     cy.get('[id=logo_homepage_link').contains('DuckDuckGo')
-    cy.get('.badge-link__title').contains('Cansado de ser rastreado online? Nós podemos ajudar.')
-    cy.get('.badge-link__btn').contains('Adicionar o DuckDuckGo ao Chrome')
+    cy.get('.badge-link__title').contains('Tired of being tracked online? We can help.')
+    cy.get('.badge-link__btn').contains('Add DuckDuckGo to Chrome')
   })
 
-  describe('Cenário 1 - Pesquisando palavra/termo e clicando na lupa', () => {
+  describe('Scenario 1 - Pesquisando palavra/termo e clicando na lupa', () => {
 
-    context('Dado que acesso a página do buscador', () => {
+    context('GIVEN acesso a pagina do buscador', () => {
 
-      context('Quando digito a palavra / termo "bitcoin" e clico na lupa', () => { // clique na lupa para buscar
+      context('WHEN digito a palavra / termo "bitcoin" E clico na lupa', () => { // clique na lupa para buscar
         
         beforeEach(() => {
           cy.get('#search_form_input_homepage')
@@ -34,7 +35,7 @@ describe('Casos de Teste - Busca na página DuckDuckGo', () => {
           cy.screenshot('after-click')
         })
 
-        it('Então vejo uma lista de resultados sobre o termo que procurei', () => {
+        it('THEN vejo uma lista de resultados sobre o termo que procurei', () => {
           cy.get('[id=header_wrapper]')
           cy.get('[id=zci-cryptocurrency]')
           cy.get('[id=links_wrapper]')
@@ -47,19 +48,20 @@ describe('Casos de Teste - Busca na página DuckDuckGo', () => {
 
   })
 
-  describe('Cenário 2 - Pesquisando palavra/termo e pressionando ENTER', () => {
+  describe('Scenario 2 - Pesquisando palavra/termo e pressionando ENTER', () => {
     
-    context('Dado que acesso a página do buscador', () => {
+    context('GIVEN que acesso a pagina do buscador', () => {
       
-      context('Quando digito um termo e pressiono ENTER', () => { // utilizando ENTER para buscar
+      context('WHEN digito um termo E pressiono ENTER', () => { // utilizando ENTER para buscar
         
         beforeEach(() => {
           cy.get('#search_form_input_homepage')
             .should('be.visible') // se retirar esse comando ele roda em modo headless
-            .type((searchData.search_subject),{enter})
+            .type(searchData.search_subject)
+            .type('{enter}')
         })
         
-        it('Então vejo uma lista de resultados sobre o termo que procurei', () => {
+        it('THEN vejo uma lista de resultados sobre o termo que procurei', () => {
           cy.get('[id=header_wrapper]')
           cy.get('[id=zci-cryptocurrency]')
           cy.get('[id=links_wrapper]')
